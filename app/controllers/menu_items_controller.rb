f02ea83ccf2b219f7b84fbd5292d7c2b118323a1
nicodmp <nicolasdmpr@gmail.com>
2025-03-29 21:37:59 -0300

    create Menu and MenuItem controllers

---
+++ app/controllers/menu_items_controller.rb
@@ -0,0 +1,57 @@
+class MenuItemsController < ApplicationController
+  before_action :set_menu_item, only: %i[ show ]
+
+  # GET /menu_items
+  def index
+    if params[:menu_id]
+      menu = Menu.find(params[:menu_id])
+      @menu_items = menu.menu_items
+    else
+      @menu_items = MenuItem.all
+    end
+
+    render json: @menu_items
+  end
+
+  # GET /menu_items/1
+  def show
+    render json: @menu_item
+  end
+
+  # POST /menu_items
+  # def create
+  #   @menu_item = MenuItem.new(menu_item_params)
+
+  #   if @menu_item.save
+  #     render json: @menu_item, status: :created, location: @menu_item
+  #   else
+  #     render json: { errors: @menu.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
+
+  # PATCH/PUT /menu_items/1
+  # def update
+  #   if @menu_item.update(menu_item_params)
+  #     render json: @menu_item
+  #   else
+  #     render json: { errors: @menu.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
+
+  # DELETE /menu_items/1
+  # def destroy
+  #   if @menu_item.destroy
+  #     head :no_content
+  #   else
+  #     { error: "Failed to delete menu item" }, status: :unprocessable_entity
+  # end
+
+  private
+    def set_menu_item
+      @menu_item = MenuItem.find(params[:id])
+    end
+
+    def menu_item_params
+      params.require(:menu_item).permit(:name, :description, :price, :category, :menu_id)
+    end
+end
---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,54 @@
+class MenusController < ApplicationController
+  before_action :set_menu, only: %i[ show ]
+
+  # GET /menus
+  def index
+    @menus = Menu.all
+
+    render json: @menus, include: :menu_items
+  end
+
+  # GET /menus/1
+  def show
+    render json: @menu, include: :menu_items
+  end
+
+  # POST /menus
+  # def create
+  #   @menu = Menu.new(menu_params)
+
+  #   if @menu.save
+  #     render json: @menu, status: :created, location: @menu
+  #   else
+  #     render json: { errors: @menu.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
+
+  # PATCH/PUT /menus/1
+  # def update
+  #   if @menu.update(menu_params)
+  #     render json: @menu
+  #   else
+  #     render json: { errors: @menu.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  # end
+
+  # DELETE /menus/1
+  # def destroy
+  #   if @menu.destroy
+  #     head :no_content
+  #   else
+  #     { error: "Failed to delete menu" }, status: :unprocessable_entity
+  #   end
+  # end
+
+  private
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+  end
+
+  def menu_params
+    params.require(:menu).permit(:name, :description, :open_time, :close_time)
+  end
+end
