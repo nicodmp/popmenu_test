49afab09902f38f5fcf5d90b8e74b17e077b93c1
nicodmp <nicolasdmpr@gmail.com>
2025-03-31 20:32:29 -0300

    create tests for restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:menus).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
 end
