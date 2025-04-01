bf746fdea8f0b1a479984b58ec9c0af077c16e29
nicodmp <nicolasdmpr@gmail.com>
2025-04-01 08:10:09 -0300

    remove menu_menu_items routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [ :index, :show ] do
-    resources :menus, shallow: true do
-      resources :menu_menu_items, only: [ :index, :show ]
-    end
+    resources :menus, shallow: true
   end
 
   resources :menu_items, only: [ :index, :show ]
-  resources :menu_menu_items, only: [ :index, :show ]
   post "/import", to: "imports#create"
 end
